65150cab6e0f7eaf71059f1c82f2c7480cfe59a3
calimanleontin <calimanleontin@gmail.com>
2016-03-02 23:07:12 +0200

    frontend

---
+++ database/migrations/2016_03_02_205921_profile.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Profile extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profile', function(Blueprint $table){
+            $table->increments('id');
+            $table->string('firstName');
+            $table->string('lastName');
+            $table->dateTime('birthday');
+            $table->string('avatar');
+            $table->integer('user_id')->unsigned()->default(0);
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('user')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
