007acac77839af83097b8557f4c5da7d8e912c7d
calimanleontin <calimanleontin@gmail.com>
2016-03-03 08:47:59 +0200

    add roles

--- app/User.php
+++ app/User.php
@@ -43,4 +43,9 @@ class User extends Authenticatable
     {
         return $this->hasMany('App\MovieComments');
     }
+
+    public function is_admin()
+    {
+        return $this->role == 'admin';
+    }
 }
