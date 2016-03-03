179d83672187a41bed19c16e5bf62eccf01aa0d9
calimanleontin <calimanleontin@gmail.com>
2016-03-03 18:23:05 +0200

    small changes on frontend

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -5,15 +5,29 @@
 @section('content')
 
     <div class="">
-        <div class="img-responsive left">
-            <img src={{$movie->Poster}}>
+        <div>
+            <div class="left img-responsive spacing-right">
+                <img src={{$movie->Poster}}>
+            </div>
+            <div class="spacing-left">
+                <h4>Year:{{$movie->Year}}</h4>
+                <h4>Rated:{{$movie->Rated}}</h4>
+                <h4>Released:{{$movie->Released}}</h4>
+                <h4>Runtime:{{$movie->Runtime}}</h4>
+                <h4>Genre:{{$movie->Genre}}</h4>
+                <h4>Director:{{$movie->Director}}</h4>
+                <h4>Writer:{{$movie->Writer}}</h4>
+                <h4>Actors:{{$movie->Actors}}</h4>
+                <br>
+                <h4>IMDB RATING:{{$movie->imdbRating}} </h4>
+                <h4>IMDB VOTES:{{$movie->imdbVotes}}</h4>
+            </div>
         </div>
-        <div class="left spacing">
-            <h4>Year:{{$movie->Year}}</h4>
-            <h4>Rated:{{$movie->Rated}}</h4>
-            <h4>Released:{{$movie->Released}}</h4>
-            <h4>Runtime:{{$movie->Runtime}}</h4>
-            <h4>Genre:{{$movie->Genre}}</h4>
+        <div>
+
         </div>
+
+
     </div>
+
 @endsection
