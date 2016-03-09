af0859104f8fa165dc8520a17872f3f26d4faec0
calimanleontin <calimanleontin@gmail.com>
2016-03-09 12:19:53 +0200

    add search bar to home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,24 @@
 @extends('app')
 @section('title')
-    @if(!empty($title))
-        {{$title}}
-    @else
-        First Page
-    @endif
+
+    <ul class="list-inline">
+        <li>
+            @if(!empty($title))
+                {{$title}}
+            @else
+                First Page
+            @endif
+        </li>
+        <li>
+                <form method="get" action="/imdbsearch/" class="">
+                    <label for="q"></label>
+                    <div class="form-group col-md-4">
+                        <input type="text" id='movies' name="q" class="long-search form-control" placeholder="Search imdb movie">
+                    </div>
+                </form>
+        </li>
+    </ul>
+
 @endsection
 @section('content')
 
